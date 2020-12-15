de7bfb1e5f491a6be90d64f5e6ae4e7cefc7bccd
Jenna <jenna@Jennas-MacBook-Pro.local>
2020-12-14 21:25:24 -0500

    idk what the heck is going on

--- module10_solution/src/public/signup.controller.js
+++ module10_solution/src/public/signup.controller.js
@@ -13,22 +13,22 @@ function SignupController(MenuService, InfoService) {
   	userDetails.pattern = "^[0-9]+$";
 
 	userDetails.submit = function(){
-	  var promise= MenuService.getShortNames(userDetails.user.menuNumber);
-	  promise.then(function(result){
-		if(result === undefined || result.length ==0 || result.status ==500){
-			userDetails.message = "No such menu number exists";
-		}
-		else{
-			userDetails.message = "";
-			var details = userDetails.user;
-			details["title"]= result.name;
-			details["description"]= result.description;
-			details["image"]= result.title;
-			InfoService.setUser(details);
-			userDetails.success = "success";
-		}
-	  });
-	};
+	  	var promise= MenuService.getShortNames(userDetails.user.menuNumber);
+	  	promise.then(function(result){
+			if(result === undefined || result.length ==0 || result.status ==500){
+				userDetails.message = "No such menu number exists";
+			}
+			else{
+				userDetails.message = "";
+				var details = userDetails.user;
+				details["title"]= result.name;
+				details["description"]= result.description;
+				details["image"]= result.title;
+				InfoService.setUser(details);
+				userDetails.success = "success";
+			}
+		});
+	  };
 
  	userDetails.checkFavorite = function(){
  		var promise= MenuService.getShortNames(userDetails.user.menuNumber);
